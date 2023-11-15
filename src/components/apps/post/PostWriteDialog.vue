48614c8c9ab24e68e4f10b95607858a67a208ac7
LeeYongho91 <skyks4848@naver.com>
2023-11-15 23:40:49 +0900

    form ui 컴포넌트 분리

--- src/components/apps/post/PostWriteDialog.vue
+++ src/components/apps/post/PostWriteDialog.vue
@@ -6,37 +6,11 @@
         <q-btn v-close-popup flat round dense icon="close"></q-btn>
       </q-toolbar>
       <q-separator />
-      <q-form>
-        <q-card-section class="q-gutter-y-sm">
-          <q-input v-model="form.title" outlined placeholder="제목" />
-          <q-select outlined v-model="form.category" :options="categories">
-            <template v-if="!form.category" #selected>
-              <span class="text-grey-7">카테고리를 선택하세요.</span>
-            </template>
-          </q-select>
-          <q-input
-            type="textarea"
-            outlined
-            placeholder="내용을 작성해주세요"
-            v-model="form.content"
-          ></q-input>
-          <q-input
-            v-model="tagField"
-            outlined
-            placeholder="태그를 입력해주세요 (입력 후 Enter)"
-            prefix="#"
-          ></q-input>
-          <q-chip outline dense removable @remove="removeTag" color="teal"
-            >vuejs</q-chip
-          >
-        </q-card-section>
-
-        <q-separator />
-        <q-card-actions align="right">
-          <q-btn flat label="취소하기" v-close-popup />
-          <q-btn flat label="저장하기" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-form>
+      <PostForm
+        v-model:title="form.title"
+        v-model:category="form.category"
+        v-model:content="form.content"
+      />
     </q-card>
   </q-dialog>
 </template>
@@ -53,15 +27,14 @@ const getInitialForm = () => ({
 <script setup>
 import { useAttrs, ref } from 'vue';
 import { getCategories } from '@/services/category';
+import PostForm from '@/components/apps/post/PostForm.vue';
 
 const categories = getCategories();
 
 const form = ref(getInitialForm());
-const tagField = ref('');
 
 const onHide = () => {
   form.value = getInitialForm();
-  tagField.value = '';
 };
 
 const removeTag = () => {};
--- src/pages/posts/[id]/edit.vue
+++ src/pages/posts/[id]/edit.vue
@@ -1,44 +1,23 @@
 <template>
-  <q-dialog persistent v-bind="$attrs" @hide="onHide">
-    <q-card :style="{ width: '600px' }">
+  <q-page padding>
+    <BaseCard>
       <q-toolbar>
         <q-toolbar-title>글쓰기</q-toolbar-title>
         <q-btn v-close-popup flat round dense icon="close"></q-btn>
       </q-toolbar>
       <q-separator />
-      <q-form>
-        <q-card-section class="q-gutter-y-sm">
-          <q-input v-model="form.title" outlined placeholder="제목" />
-          <q-select outlined v-model="form.category" :options="categories">
-            <template v-if="!form.category" #selected>
-              <span class="text-grey-7">카테고리를 선택하세요.</span>
-            </template>
-          </q-select>
-          <q-input
-            type="textarea"
-            outlined
-            placeholder="내용을 작성해주세요"
-            v-model="form.content"
-          ></q-input>
-          <q-input
-            v-model="tagField"
-            outlined
-            placeholder="태그를 입력해주세요 (입력 후 Enter)"
-            prefix="#"
-          ></q-input>
-          <q-chip outline dense removable @remove="removeTag" color="teal"
-            >vuejs</q-chip
-          >
-        </q-card-section>
-
-        <q-separator />
-        <q-card-actions align="right">
-          <q-btn flat label="취소하기" v-close-popup />
-          <q-btn flat label="저장하기" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-form>
-    </q-card>
-  </q-dialog>
+      <PostForm
+        v-model:title="form.title"
+        v-model:category="form.category"
+        v-model:content="form.content"
+      >
+        <template #actions>
+          <q-btn flat label="취소" v-close-popup />
+          <q-btn flat label="저장" color="primary" v-close-popup />
+        </template>
+      </PostForm>
+    </BaseCard>
+  </q-page>
 </template>
 
 <script>
@@ -52,19 +31,21 @@ const getInitialForm = () => ({
 
 <script setup>
 import { useAttrs, ref } from 'vue';
-import { getCategories } from '@/services/category';
 
-const categories = getCategories();
+import BaseCard from '@/components/base/BaseCard.vue';
+import PostForm from '@/components/apps/post/PostForm.vue';
 
 const form = ref(getInitialForm());
-const tagField = ref('');
 
 const onHide = () => {
   form.value = getInitialForm();
   tagField.value = '';
 };
-
-const removeTag = () => {};
 </script>
 
 <style lang="scss" scoped></style>
+
+<route lang="yaml">
+meta:
+  width: 800px
+</route>
