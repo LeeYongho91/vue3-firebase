8d70808706095b0a0d5ff9ff43a5121a7877d825
LeeYongho91 <skyks4848@naver.com>
2023-11-17 23:18:16 +0900

    게시글 수정 ui

--- src/pages/mypage/bookmark.vue
+++ src/pages/mypage/bookmark.vue
@@ -1,9 +1,21 @@
-<template>
-  <div>
-    <div class="text-h5">bookmark</div>
-  </div>
-</template>
+<template><PostList :items="posts" /></template>
 
-<script setup></script>
+<script setup>
+import PostList from '@/components/apps/post/PostList.vue';
+
+const posts = Array.from(Array(3), (_, index) => ({
+  id: 'A' + index,
+  title: 'Vue3 Firebase 강의' + index,
+  content:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit.pti. Lorem ipsum dolor sit amet, consectetur adipisicing elit.pti.Lorem ipsum dolor sit amet, consectetur adipisicing elit.pti.',
+  readCount: 1,
+  commentCount: 2,
+  likeCount: 3,
+  bookmarkCount: 4,
+  tags: ['html', 'css', 'javascript'],
+  uid: 'uid',
+  category: '카테고리',
+}));
+</script>
 
 <style lang="scss" scoped></style>
