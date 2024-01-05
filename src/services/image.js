72413912a58c6de91a562cdb9c2c8ac0f585f9a6
LeeYongho91 <skyks4848@naver.com>
2024-01-06 02:26:48 +0900

    feat: firebase function 설정

--- src/services/image.js
+++ src/services/image.js
@@ -1,15 +1,23 @@
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { storage } from '@/boot/firebase';
 import { v4 as uuidv4 } from 'uuid';
+import {readAndCompressImage} from 'browser-image-resizer';
 
 export async function uploadImage(file) {
   const filename = `images/${uuidv4()}.${getExtension(file.name)}`
   const storageRef = ref(storage, filename);
-  const uploadResult = await uploadBytes(storageRef, file);
+  const thumbnail = await compressImage(file);
+  const uploadResult = await uploadBytes(storageRef, thumbnail);
   const downloadURL = await getDownloadURL(uploadResult.ref);
   return downloadURL;
 }
 
 function getExtension(filename) {
   return filename.split('.').pop();
+}
+
+async function compressImage(file) {
+  return readAndCompressImage(file, {
+    quality: 0.8
+  })
 }
